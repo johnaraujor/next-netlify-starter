3d635d5a8f87a5cdd83841d42905f7e6dfdc7386
John A. Araújo <JOHNB2932@GMAIL.COM>
2021-11-23 03:45:42 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,10 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <Header title="Welcome to my app!" />
-
-        </p>
       </main>
-
+    
       <Footer />
     </div>
   )
